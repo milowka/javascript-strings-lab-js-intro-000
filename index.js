07c7556bea089f00076259ea28788b152995499d
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-08-21 14:40:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "greeting," + specialGuest + "!";
+greeting = "Hello,"
+var greetSpecialGuest = "greeting" + specialGuest + "!";
 
 var topic = "Let's talk about space.";
 
